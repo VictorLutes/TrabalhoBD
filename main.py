e95bf5c407a6c71e1b1b774054d5e32c71dc8c0b
VictorLutes <victorlutes@usp.br>
2022-06-22 19:00:47 -0300

    python fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cx_Oracle
 dsn_tns = cx_Oracle.makedsn('Host Name', 'Port Number', service_name='Service Name') # if needed, place an 'r' before any parameter in order to address special characters such as '\'.
 conn = cx_Oracle.connect(user=r'User Name', password='Personal Password', dsn=dsn_tns) # if needed, place an 'r' before any parameter in order to address special characters such as '\'. For example, if your user name contains '\', you'll need to place 'r' before the user name: user=r'User Name'
 
-c = conn.cursor()
+cur = conn.cursor()
 
 
 def inserir():
