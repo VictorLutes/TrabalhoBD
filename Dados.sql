c85e86dc49a804004bd4c659578e31d603920963
Victor Paulo Cruz Lutes <61762690+VictorLutes@users.noreply.github.com>
2022-06-22 14:18:25 -0300

    Add files via upload

---
+++ Dados.sql
@@ -0,0 +1,124 @@
+INSERT INTO Usuario (email, nome, senha) VALUES ('victor@lutes.com', 'victor lutes', 'senha');
+INSERT INTO Usuario (email, nome, senha) VALUES ('john@doe.com', 'john doe', '1234');
+INSERT INTO Usuario (email, nome, senha) VALUES ('dannydevito@gmail.com', 'Danny Devito', 'iasip');
+
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('victor@lutes.com', 1);
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('victor@lutes.com', 2);
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('victor@lutes.com', 3);
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('victor@lutes.com', 4);
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('dannydevito@gmail.com', 4);
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('dannydevito@gmail.com', 5);
+INSERT INTO MarcaParaAssistir (usuario, midia) VALUES ('john@doe.com', 6);
+
+
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (5, 'victor@lutes.com', TO_DATE('2022/04/30 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 100);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (6, 'victor@lutes.com', TO_DATE('2022/04/30 19:00:00', 'YYYY/MM/DD HH24:MI:SS'), 200);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (7, 'victor@lutes.com', TO_DATE('2022/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 300);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (1, 'dannydevito@gmail.com', TO_DATE('2022/04/30 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 100);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (2, 'dannydevito@gmail.com', TO_DATE('2022/04/30 19:00:00', 'YYYY/MM/DD HH24:MI:SS'), 200);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (3, 'dannydevito@gmail.com', TO_DATE('2022/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 300);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (1, 'john@doe.com', TO_DATE('2022/04/30 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 100);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (2, 'john@doe.com', TO_DATE('2022/04/30 19:00:00', 'YYYY/MM/DD HH24:MI:SS'), 200);
+INSERT INTO Visto (midiaLicenciada,usuario, data, minuto) VALUES (3, 'john@doe.com', TO_DATE('2022/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 300);
+
+INSERT INTO Pais (nome) VALUES ('BRASIL');
+INSERT INTO Pais (nome) VALUES ('CANADA');
+INSERT INTO Pais (nome) VALUES ('CHINA');
+INSERT INTO Pais (nome) VALUES ('JAPAO');
+INSERT INTO Pais (nome) VALUES ('MEXICO');
+INSERT INTO Pais (nome) VALUES ('FRANCA');
+INSERT INTO Pais (nome) VALUES ('ESTADOS UNIDOS');
+
+INSERT INTO EstaEm (usuario, pais) VALUES ('dannydevito@gmail.com', 'ESTADOS UNIDOS');
+INSERT INTO EstaEm (usuario, pais) VALUES ('dannydevito@gmail.com', 'BRASIL');
+INSERT INTO EstaEm (usuario, pais) VALUES ('victor@lutes.com', 'BRASIL');
+INSERT INTO EstaEm (usuario, pais) VALUES ('john@doe.com', 'BRASIL');
+
+INSERT INTO Utiliza (usuario, vpn) VALUES ('john@doe.com', 'NORDVPN');
+INSERT INTO Utiliza (usuario, vpn) VALUES ('victor@lutes.com', 'EXPRESSVPN');
+
+INSERT INTO VPN (nome, preco) VALUES ('EXPRESSVPN', 25);
+INSERT INTO VPN (nome, preco) VALUES ('NORDVPN', 20);
+
+INSERT INTO Determina (vpn, pais) VALUES ('NORDVPN', 'ESTADOS UNIDOS');
+INSERT INTO Determina (vpn, pais) VALUES ('NORDVPN', 'BRASIL');
+INSERT INTO Determina (vpn, pais) VALUES ('EXPRESSVPN', 'BRASIL');
+INSERT INTO Determina (vpn, pais) VALUES ('EXPRESSVPN', 'CANADA');
+
+INSERT INTO Assina (usuario, streaming) VALUES ('john@doe.com', 'NETFLIX');
+INSERT INTO Assina (usuario, streaming) VALUES ('victor@lutes.com', 'STREMIO');
+INSERT INTO Assina (usuario, streaming) VALUES ('victor@lutes.com', 'NETFLIX');
+INSERT INTO Assina (usuario, streaming) VALUES ('victor@lutes.com', 'HULU');
+INSERT INTO Assina (usuario, streaming) VALUES ('dannydevito@gmail.com', 'HULU');
+INSERT INTO Assina (usuario, streaming) VALUES ('dannydevito@gmail.com', 'HBO');
+
+INSERT INTO Streaming (nome, preco) VALUES ('NETFLIX', 25);
+INSERT INTO Streaming (nome, preco) VALUES ('HULU', 20);
+INSERT INTO Streaming (nome, preco) VALUES ('HBO', 30);
+INSERT INTO Streaming (nome, preco) VALUES ('STREMIO', 0);
+
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (1, 1, 'NETFLIX', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (2, 1, 'NETFLIX', 'ESTADOS UNIDOS');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (3, 1, 'HULU', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (4, 1, 'HBO', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (5, 1, 'STREMIO', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (6, 2, 'NETFLIX', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (7, 3, 'NETFLIX', 'ESTADOS UNIDOS');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (8, 2, 'HULU', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (9, 4, 'HBO', 'BRASIL');
+INSERT INTO MidiaLicenciada (id, midia, streaming, pais) VALUES (10, 5, 'STREMIO', 'BRASIL');
+
+INSERT INTO Midia (id, titulo, diretor, data, descricao, tituloEstrangeiro, tipo) VALUES (1, 'IN BRUGES', 'MATRIN MCDOUNAGH', TO_DATE('2016/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 'MUITO BOM E ENGRACADO', 'NA MIRA DO CHEFE', 'FILME');
+INSERT INTO Midia (id, titulo, diretor, data, descricao, tituloEstrangeiro, tipo) VALUES (2, 'BARRY', 'BILL HADER', TO_DATE('2016/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 'MUITO BOM E ENGRACADO', 'BARRY', 'SERIE');
+INSERT INTO Midia (id, titulo, diretor, data, descricao, tituloEstrangeiro, tipo) VALUES (3, 'IN THE MOOD FOR LOVE', 'WONG KAR WAI', TO_DATE('2016/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 'MUITO BOM E TRISTE', 'AMOR A FLOR DA PELE', 'FILME');
+INSERT INTO Midia (id, titulo, diretor, data, descricao, tituloEstrangeiro, tipo) VALUES (4, 'DR WHO', 'STEVEN MOFFAT', TO_DATE('2016/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 'MUITO BOM E SCIFI', 'DR WHO', 'SERIE');
+INSERT INTO Midia (id, titulo, diretor, data, descricao, tituloEstrangeiro, tipo) VALUES (5, 'LETTERKENNY', 'JARED KEESO', TO_DATE('2016/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 'MUITO BOM E ENGRACADO', 'LETTERKENNY', 'SERIE');
+INSERT INTO Midia (id, titulo, diretor, data, descricao, tituloEstrangeiro, tipo) VALUES (6, 'ITS ALWAYS SUNNY IN PHILADELPHIA', 'ROB MCELHENNEY', TO_DATE('2016/05/02 16:00:00', 'YYYY/MM/DD HH24:MI:SS'), 'MUITO BOM E ENGRACADO', 'IASIP', 'SERIE');
+
+INSERT INTO Photo(midia, enderecoPhoto) VALUES(1, DEFAULT);
+INSERT INTO Photo(midia, enderecoPhoto) VALUES(2, DEFAULT);
+INSERT INTO Photo(midia, enderecoPhoto) VALUES(3, DEFAULT);
+INSERT INTO Photo(midia, enderecoPhoto) VALUES(4, DEFAULT);
+INSERT INTO Photo(midia, enderecoPhoto) VALUES(5, DEFAULT);
+INSERT INTO Photo(midia, enderecoPhoto) VALUES(6, DEFAULT);
+
+INSERT INTO Genero(midia, nomeGenero) VALUES(1, 'COMEDIA');
+INSERT INTO Genero(midia, nomeGenero) VALUES(1, 'DRAMA');
+INSERT INTO Genero(midia, nomeGenero) VALUES(2, 'COMEDIA');
+INSERT INTO Genero(midia, nomeGenero) VALUES(3, 'ROMANCE');
+INSERT INTO Genero(midia, nomeGenero) VALUES(4, 'SCIFI');
+INSERT INTO Genero(midia, nomeGenero) VALUES(5, 'COMEDIA');
+INSERT INTO Genero(midia, nomeGenero) VALUES(6, 'COMEDIA');
+
+INSERT INTO Filme(id, duracao) VALUES(1, 120);
+INSERT INTO Filme(id, duracao) VALUES(3, 120);
+
+INSERT INTO ElencoFilme(filme, nome) VALUES(1, 'COLIN FARRELL');
+INSERT INTO ElencoFilme(filme, nome) VALUES(3, 'TONY LEUNG');
+
+INSERT INTO DubLegFilme(filme, linguagem) VALUES(1, 'ESPANHOL');
+INSERT INTO DubLegFilme(filme, linguagem) VALUES(3, 'INGLES');
+
+INSERT INTO Serie(id, nroTemporadas, nroEps) VALUES(2, 3, 12);
+INSERT INTO Serie(id, nroTemporadas, nroEps) VALUES(4, 15, 12);
+INSERT INTO Serie(id, nroTemporadas, nroEps) VALUES(5, 9, 10);
+INSERT INTO Serie(id, nroTemporadas, nroEps) VALUES(6, 13, 8);
+
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 1, 1, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 1, 2, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 1, 3, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 1, 4, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 1, 5, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 2, 1, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(2, 3, 1, 30);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(4, 1, 1, 20);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(5, 1, 1, 50);
+INSERT INTO Episodio(serie, nroEp, nroTemp, duracao) VALUES(6, 1, 1, 20);
+
+INSERT INTO ElencoEpisodio(serie, nroEp, nroTemp, nome) VALUES(2, 1, 1, 'BILL HADER');
+INSERT INTO ElencoEpisodio(serie, nroEp, nroTemp, nome) VALUES(4, 1, 1, 'DAVID TENNANT');
+INSERT INTO ElencoEpisodio(serie, nroEp, nroTemp, nome) VALUES(5, 1, 1, 'JARED KEESO');
+INSERT INTO ElencoEpisodio(serie, nroEp, nroTemp, nome) VALUES(6, 1, 1, 'CHARLIE DAY');
+
+INSERT INTO DubLegEpisodio(serie, nroEp, nroTemp, linguagem) VALUES(2, 1, 1, 'PORTUGUES');
+INSERT INTO DubLegEpisodio(serie, nroEp, nroTemp, linguagem) VALUES(4, 1, 1, 'FRANCES');
---
+++ Esquema.sql
@@ -0,0 +1,172 @@
+CREATE TABLE VPN (
+   nome VARCHAR2(20),
+   preco NUMBER(4),
+   CONSTRAINT pk_vpn PRIMARY KEY(nome)
+);
+
+CREATE TABLE Streaming (
+   nome VARCHAR2(20),
+   preco NUMBER(4),
+   CONSTRAINT pk_streaming PRIMARY KEY(nome)
+);
+
+CREATE TABLE Pais (
+   nome VARCHAR2(20),
+   CONSTRAINT pk_pais PRIMARY KEY(nome)
+);
+
+CREATE TABLE Usuario (
+   email VARCHAR2(20),
+   nome VARCHAR2(20) NOT NULL,
+   senha VARCHAR2(20) NOT NULL,
+   CONSTRAINT pk_usuario PRIMARY KEY(email)
+);
+
+CREATE TABLE EstaEm (
+   usuario VARCHAR2(20),
+   pais VARCHAR2(20),
+   CONSTRAINT pk_estaem PRIMARY KEY(usuario, pais),
+   CONSTRAINT fk_estaem FOREIGN KEY(usuario) REFERENCES Usuario(email),
+   CONSTRAINT fk_estaem2 FOREIGN KEY(pais) REFERENCES Pais(nome)
+);
+
+
+CREATE TABLE Utiliza (
+   usuario VARCHAR2(20),
+   vpn VARCHAR2(20),
+   CONSTRAINT pk_utiliza PRIMARY KEY(usuario, vpn),
+   CONSTRAINT fk_utiliza FOREIGN KEY(vpn) REFERENCES VPN(nome),
+   CONSTRAINT fk_utiliza2 FOREIGN KEY(usuario) REFERENCES Usuario(email)
+);
+
+
+CREATE TABLE Assina (
+   usuario VARCHAR2(20),
+   streaming VARCHAR2(20),
+   CONSTRAINT pk_assina PRIMARY KEY(usuario, streaming),
+   CONSTRAINT fk_assina FOREIGN KEY(streaming) REFERENCES Streaming(nome)
+);
+
+CREATE TABLE Determina (
+   vpn VARCHAR2(20),
+   pais VARCHAR2(20),
+   CONSTRAINT pk_determina PRIMARY KEY(vpn, pais),
+   CONSTRAINT fk_determina1 FOREIGN KEY(vpn) REFERENCES VPN(nome),
+   CONSTRAINT fk_determina2 FOREIGN KEY(pais) REFERENCES Pais(nome)
+);
+
+CREATE TABLE Midia (
+   id NUMBER(4),
+   titulo VARCHAR2(20) NOT NULL,
+   diretor VARCHAR2(20) NOT NULL,
+   data DATE, 
+   descricao VARCHAR2(100),
+   tituloEstrangeiro VARCHAR2(20),
+   tipo VARCHAR2(5),
+   CONSTRAINT pk_midia PRIMARY KEY(id),
+   CONSTRAINT sk_midia UNIQUE(titulo, diretor),
+   CONSTRAINT ck_tipo CHECK(UPPER(tipo) IN ('SERIE', 'FILME'))
+);
+
+CREATE TABLE Photo (
+   midia NUMBER(4),
+   enderecoPhoto VARCHAR2(20),
+   CONSTRAINT pk_photo PRIMARY KEY(midia, enderecoPhoto),
+   CONSTRAINT fk_photo FOREIGN KEY(midia) REFERENCES Midia(id)
+);
+
+CREATE TABLE Genero (
+   midia NUMBER(4),
+   nomeGenero VARCHAR2(20),
+   CONSTRAINT pk_genero PRIMARY KEY(midia, nomeGenero),
+   CONSTRAINT fk_genero FOREIGN KEY(midia) REFERENCES Midia(id)
+);
+
+CREATE TABLE MidiaLicenciada (
+   id NUMBER(4),
+   midia NUMBER(4) NOT NULL,
+   streaming VARCHAR2(20) NOT NULL,
+   pais VARCHAR2(20) NOT NULL,
+   CONSTRAINT pk_midialicenciada PRIMARY KEY(id),
+   CONSTRAINT sk_midialicenciada UNIQUE(midia, streaming, pais),
+   CONSTRAINT fk_midialicenciada1 FOREIGN KEY(midia) REFERENCES Midia(id),
+   CONSTRAINT fk_midialicenciada2 FOREIGN KEY(streaming) REFERENCES Streaming(nome),
+   CONSTRAINT fk_midialicenciada3 FOREIGN KEY(pais) REFERENCES Pais(nome)
+);
+
+
+
+CREATE TABLE MarcaParaAssistir (
+   usuario VARCHAR2(20),
+   midia NUMBER(4),
+   CONSTRAINT pk_marcaparaassistir PRIMARY KEY(usuario, midia),
+   CONSTRAINT fk_marcaparaassistir1 FOREIGN KEY(usuario) REFERENCES Usuario(email),
+   CONSTRAINT fk_marcaparaassistir2 FOREIGN KEY(midia) REFERENCES Midia(id)
+);
+
+CREATE TABLE Visto (
+   midiaLicenciada NUMBER(4),
+   usuario VARCHAR2(20),
+   data DATE, 
+   minuto NUMBER(4),
+   CONSTRAINT pk_visto PRIMARY KEY(midiaLicenciada, usuario, data),
+   CONSTRAINT fk_visto1 FOREIGN KEY(midiaLicenciada) REFERENCES MidiaLicenciada(id),
+   CONSTRAINT fk_visto2 FOREIGN KEY(usuario) REFERENCES Usuario(email),
+   CONSTRAINT ck_minuto CHECK(minuto>0 and minuto<1200)
+);
+
+CREATE TABLE Filme (
+   id NUMBER(4),
+   duracao NUMBER(3),
+   CONSTRAINT pk_filme PRIMARY KEY(id),
+   CONSTRAINT fk_filme FOREIGN KEY(id) REFERENCES Midia(id)
+);
+
+CREATE TABLE ElencoFilme (
+   filme NUMBER(4),
+   nome NUMBER(3),
+   CONSTRAINT pk_elencofilme PRIMARY KEY(filme, nome),
+   CONSTRAINT fk_elencofilme FOREIGN KEY(filme) REFERENCES Filme(id)
+);
+
+CREATE TABLE DubLegFilme (
+   filme NUMBER(4),
+   linguagem NUMBER(3),
+   CONSTRAINT pk_dubfilme PRIMARY KEY(filme, linguagem),
+   CONSTRAINT fk_dubfilme FOREIGN KEY(filme) REFERENCES Filme(id)
+);
+
+CREATE TABLE Serie (
+   id NUMBER(4),
+   nroTemporadas NUMBER(2),
+   nroEps NUMBER(3),
+   CONSTRAINT pk_serie PRIMARY KEY(id),
+   CONSTRAINT fk_serie FOREIGN KEY(id) REFERENCES Midia(id)
+);
+
+CREATE TABLE Episodio (
+   serie NUMBER(4),
+   nroTemp NUMBER(2),
+   nroEp NUMBER(3),
+   duracao NUMBER(3),
+   CONSTRAINT pk_episodio PRIMARY KEY(serie, nroTemp, nroEp),
+   CONSTRAINT fk_episodio FOREIGN KEY(serie) REFERENCES Serie(id)
+);
+
+CREATE TABLE ElencoEpisodio (
+   serie NUMBER(4),
+   nroTemp NUMBER(2),
+   nroEp NUMBER(3),
+   nome NUMBER(3),
+   CONSTRAINT pk_elencoepisodio PRIMARY KEY(serie, nroTemp, nroEp, nome),
+   CONSTRAINT fk_elencoepisodio FOREIGN KEY(serie, nroTemp, nroEp) REFERENCES Episodio(serie, nroTemp, nroEp)
+);
+
+CREATE TABLE DubLegEpisodio (
+   serie NUMBER(4),
+   nroTemp NUMBER(2),
+   nroEp NUMBER(3),
+   linguagem NUMBER(3),
+   CONSTRAINT pk_dubepisodio PRIMARY KEY(serie, nroTemp, nroEp, linguagem),
+   CONSTRAINT fk_dubepisodio FOREIGN KEY(serie, nroTemp, nroEp) REFERENCES Episodio(serie, nroTemp, nroEp)
+);
