d73740d47c31626c6afdfd647e4812f9d5601a8f
VictorLutes <victorlutes@usp.br>
2022-06-22 19:33:24 -0300

    adding inputs for diferent tables

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cur = conn.cursor()
 
 
 def inserir():
-    print("Tabelas: 1-VPN, 2-Streaming, 3-Pais, 4-Usuario, 5-EstaEm, 6-Utiliza, 7-Assina, 7-Determina, 9-Midia, 10-Photo, \
+    print("Tabelas: 1-VPN, 2-Streaming, 3-Pais, 4-Usuario, 5-EstaEm, 6-Utiliza, 7-Assina, 8-Determina, 9-Midia, 10-Photo, \
         11-Genero, 12-MidiaLicenciada,  13-MarcaParaAssistir, 14-Visto, 15-Filme, 16-ElencoFilme, 17-DubLegFilme, \
         19-Serie, 20-Episodio, 21-ElencoEpisodio, 22-DubLegEpisodio")
     tabela=input("digite o numero da tabela que voce quer inserir: ")
@@ -23,9 +23,9 @@ def inserir():
         nome=input("digite o nome do pais: ")
 
     elif(tabela==4):
-        email=input("digite o email da Usuario: ")
-        nome=input("digite o nome da Usuario: ")
-        senha=input("digite o senha da Usuario: ")
+        email=input("digite o email do Usuario: ")
+        nome=input("digite o nome do Usuario: ")
+        senha=input("digite o senha do Usuario: ")
 
     elif(tabela==5):
         usuario=input("digite o usuario da EstaEm: ")
@@ -36,12 +36,24 @@ def inserir():
         vpn=input("digite a vpn da Utiliza: ")
 
     elif(tabela==7):
-        usuario=input("digite o usuario da EstaEm: ")
-        pais=input("digite o pais da EstaEm: ")
-        
+        usuario=input("digite o usuario da Assina: ")
+        streaming=input("digite o streaming da Assina: ")
+
     elif(tabela==8):
+        vpn=input("digite a vpn da Determina: ")
+        pais=input("digite o pais da Determina: ")
+
     elif(tabela==9):
+        ident=input("digite id da  Midia: ")
+        titulo=input("digite o titulo da Midia: ")
+        diretor=input("digite o diretor da Midia: ")
+        data=input("digite a data da Midia: ")
+        descricao=input("digite a descricao da Midia: ")
+        tituloEstrangeiro=input("digite o tituloEstrangeiro da Midia: ")
+        tipo=input("digite o tipo da Midia (filme ou serie): ")
+
     elif(tabela==10):
+        
     elif(tabela==11):
     elif(tabela==12):
     elif(tabela==13):
