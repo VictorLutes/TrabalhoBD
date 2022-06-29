088a53242db4d27fcd4fb20c2057b75cb9b02718
VictorLutes <victorlutes@usp.br>
2022-06-29 15:06:53 -0300

    comentarios em busca

--- Buscas.sql
+++ Buscas.sql
@@ -21,21 +21,18 @@ Buscar qual usuario mais assistiu algum genero/plataforma/nroMidias
 */
 
 
-/*Filtrar por genero:
-varGenero="DRAMA";*/
+/*Filtrar por genero:*/
 SELECT M.titulo, M.diretor FROM
     Midia M JOIN Genero G ON G.midia=M.id WHERE G.nomeGenero='genero buscado';
     
-/*filtrar por linguagem:
-varLinguagem="PORTUGUES";*/
+/*filtrar por linguagem:*/
 SELECT M.titulo, M.diretor FROM
     Midia M JOIN DubLegEpisodio D ON D.serie=M.id WHERE D.linguagem='linguagem buscada'
     UNION
 SELECT M.titulo, M.diretor FROM
     Midia M JOIN DubLegFilme D ON D.filme=M.id WHERE D.linguagem='linguagem buscada';
 
-/*buscar filmes e series com um ator:
-varNomeAtor="JOHN HAMM";*/
+/*buscar filmes e series com um ator:*/
 SELECT M.titulo, M.diretor FROM
     Midia M JOIN ElencoEpisodio E ON E.serie=M.id WHERE E.nome='ator buscado'
     UNION
@@ -43,9 +40,7 @@ SELECT M.titulo, M.diretor FROM
     Midia M JOIN ElencoFilme E ON E.filme=M.id WHERE E.nome='ator buscado';
 
 
-/*Mostrar as plataformas com mais shows que ele marcou para assistir em um pais:
-varUsuario="victor@lutes.com";
-varPais;*/
+/*Mostrar as plataformas com mais shows que ele marcou para assistir em um pais:*/
 SELECT S.nome, count(MPA.midia) FROM Streaming S 
     JOIN MidiaLicenciada ML ON S.nome=ML.streaming 
     JOIN MarcaParaAssistir MPA ON MPA.midia=ML.midia 
@@ -54,17 +49,13 @@ SELECT S.nome, count(MPA.midia) FROM Streaming S
     ORDER BY count(MPA.midia) DESC;
 
 
-/*selecionar todas as plataformas que tem um filme num determinado pais:
-varPais;
-varNomeMidia;
-varNomeDiretor;*/
+/*selecionar todas as plataformas que tem um filme num determinado pais:*/
 SELECT S.nome FROM Streaming S 
     JOIN MidiaLicenciada ML ON S.nome=ML.streaming 
     JOIN Midia M ON M.id=ML.midia 
     WHERE ML.pais='pais buscado' AND M.titulo='titulo buscado' AND M.diretor='diretor buscado';
 
 /*selecionar streaming que possui a mídia buscada pelo usuário:*/
-
 SELECT str.nome, ML.pais FROM Streaming str
     INNER JOIN MidiaLicenciada ML ON str.nome = ML.streaming
     INNER JOIN Midia M ON M.id = ML.midia
