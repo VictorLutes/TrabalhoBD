d7222aa2d8ac25fa5385c7d025dd96121916a672
VictorLutes <victorlutes@usp.br>
2022-06-29 14:34:47 -0300

    mudei as variaveis em busca.sql por strings

--- Buscas.sql
+++ Buscas.sql
@@ -24,23 +24,23 @@ Buscar qual usuario mais assistiu algum genero/plataforma/nroMidias
 /*Filtrar por genero:
 varGenero="DRAMA";*/
 SELECT M.titulo, M.diretor FROM
-    Midia M JOIN Genero G ON G.midia=M.id WHERE G.nomeGenero=varGenero;
+    Midia M JOIN Genero G ON G.midia=M.id WHERE G.nomeGenero='genero buscado';
     
 /*filtrar por linguagem:
 varLinguagem="PORTUGUES";*/
 SELECT M.titulo, M.diretor FROM
-    Midia M JOIN DubLegEpisodio D ON D.serie=M.id WHERE D.linguagem=varLinguagem
+    Midia M JOIN DubLegEpisodio D ON D.serie=M.id WHERE D.linguagem='linguagem buscada'
     UNION
 SELECT M.titulo, M.diretor FROM
-    Midia M JOIN DubLegFilme D ON D.filme=M.id WHERE D.linguagem=varLinguagem;
+    Midia M JOIN DubLegFilme D ON D.filme=M.id WHERE D.linguagem='linguagem buscada';
 
 /*buscar filmes e series com um ator:
 varNomeAtor="JOHN HAMM";*/
 SELECT M.titulo, M.diretor FROM
-    Midia M JOIN ElencoEpisodio E ON E.serie=M.id WHERE E.nome=varNomeAtor
+    Midia M JOIN ElencoEpisodio E ON E.serie=M.id WHERE E.nome='ator buscado'
     UNION
 SELECT M.titulo, M.diretor FROM
-    Midia M JOIN ElencoFilme E ON E.filme=M.id WHERE E.nome=varNomeAtor;
+    Midia M JOIN ElencoFilme E ON E.filme=M.id WHERE E.nome='ator buscado';
 
 
 /*Mostrar as plataformas com mais shows que ele marcou para assistir em um pais:
@@ -49,7 +49,7 @@ varPais;*/
 SELECT S.nome, count(MPA.midia) FROM Streaming S 
     JOIN MidiaLicenciada ML ON S.nome=ML.streaming 
     JOIN MarcaParaAssistir MPA ON MPA.midia=ML.midia 
-    WHERE MPA.usuario=varUsuario AND ML.pais=varPais
+    WHERE MPA.usuario='email buscado' AND ML.pais='pais buscado'
     GROUP BY S.nome HAVING count(MPA.midia)>0
     ORDER BY count(MPA.midia) DESC;
 
@@ -61,7 +61,7 @@ varNomeDiretor;*/
 SELECT S.nome FROM Streaming S 
     JOIN MidiaLicenciada ML ON S.nome=ML.streaming 
     JOIN Midia M ON M.id=ML.midia 
-    WHERE ML.pais=varPais AND M.titulo=varNomeMidia AND M.diretor=varNomeDiretor;
+    WHERE ML.pais='pais buscado' AND M.titulo='titulo buscado' AND M.diretor='diretor buscado';
 
 /*selecionar streaming que possui a mídia buscada pelo usuário:*/
 
